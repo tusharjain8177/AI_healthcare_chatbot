c5e74743f4b4d433a2692ce11e38aaeb2d6ad447
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2023-02-13 00:08:49 +0530

    hi

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
+from get_disease import get_disease
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods = ['POST'])
 def index():
     data = request.get_json()
-    print(data)
+    # print(data)
+    symptoms_name = data['queryResult']['parameters']['symptoms_name']
+    disese_name, medicines, doctor = get_disease(symptoms_name)
+    response = {
+        'fulfillmentText': "You have {}.You should take these medicines: {}. You should consult these doctors: {}".format(disese_name, medicines, doctor)
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
