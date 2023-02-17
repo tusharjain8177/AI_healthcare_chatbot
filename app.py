ac668092c15f936fb4c9a691faac81041a2187a0
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2023-02-17 18:05:03 +0530

    hi

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request, jsonify
-from get_disease import get_disease
+import pickle
+import pandas as pd
+
+df1 = pd.read_csv('datasets/symptom_precaution.csv')
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+data = pickle.load(open('data.pkl', 'rb'))
+
+d = pickle.load(open('d.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -8,11 +17,26 @@ def index():
     data = request.get_json()
     # print(data)
     symptoms_name = data['queryResult']['parameters']['symptoms_name']
-    disese_name, medicines, doctor = get_disease(symptoms_name)
+    disese_name = get_disease(symptoms_name)
     response = {
-        'fulfillmentText': "You have {}.You should take these medicines: {}. You should consult these doctors: {}".format(disese_name, medicines, doctor)
+        'fulfillmentText': "You have {}.".format(disese_name),
     }
     return jsonify(response)
+
+def get_disease(symptoms_name):
+    new_data = []
+    for i in range(len(data[0])):
+        if d.columns[i] in symptoms_name:
+            new_data.append(df1[df1['Symptom'] == symptoms_name[i]]['weight'].values[0])
+        else:
+            new_data.append(0)
+    
+    disease = model.predict([new_data])[0]
+    return disease
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
