d2084204c8cc78d80759856423b6ad91f8629c8a
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2023-02-17 18:31:29 +0530

    hi

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 data = pickle.load(open('data.pkl', 'rb'))
 
-d_dict = pickle.load(open('d.pkl', 'rb'))
-d = pd.DataFrame(d_dict)
+d = pd.read_csv('datasets/symptom_severity.csv')
 
 app = Flask(__name__)
 
