e66ee40b98ded4d199811ac9e8efeedec27fd271
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2023-02-18 13:29:59 +0530

    hi

--- app.py
+++ app.py
@@ -1,42 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-import pickle
-import pandas as pd
-
-df1 = pd.read_csv(r'datasets/Symptom-severity.csv')
-
-model = pickle.load(open('model.pkl', 'rb'))
-
-data = pickle.load(open('data.pkl', 'rb'))
-
-d = pd.read_csv(r'datasets/d.csv')
+from get_disease import dosomething
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
-    # print(data)
-    symptoms_name = data['queryResult']['parameters']['symptoms_name']
-    disese_name = get_disease(symptoms_name)
+    symptom = data['queryResult']['parameters']['symptoms_name']
+    output = dosomething(symptom)
+
     response = {
-        'fulfillmentText': "You have {}.".format(disese_name),
+        'fulfillmentText':"You may have {}.".format(output[0])
     }
     return jsonify(response)
 
-def get_disease(symptoms_name):
-    new_data = []
-    for i in range(len(data[0])):
-        if d.columns[i] in symptoms_name:
-            new_data.append(df1[df1['Symptom'] == symptoms_name[i]]['weight'].values[0])
-        else:
-            new_data.append(0)
-    
-    disease = model.predict([new_data])[0]
-    return disease
 
 
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
---
+++ get_disease.py
@@ -0,0 +1,25 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import csv,numpy as np,pandas as pd
+import os
+import pickle
+
+data = pd.read_csv(os.path.join("datasets", "Training.csv"))
+df = pd.DataFrame(data)
+model = pickle.load(open(os.path.join("models", "model.pkl"), "rb"))
+indices = [i for i in range(132)]
+symptoms = df.columns.values[:-1]
+
+dictionary = dict(zip(symptoms,indices))
+
+def dosomething(symptom):
+    user_input_symptoms = symptom
+    user_input_label = [0 for i in range(132)]
+    for i in user_input_symptoms:
+        idx = dictionary[i]
+        user_input_label[idx] = 1
+
+    user_input_label = np.array(user_input_label)
+    user_input_label = user_input_label.reshape((-1,1)).transpose()
+    return(model.predict(user_input_label))
+
